00e9198b7deace6d7c36e4008831e85680afe579
Yash Jain <yjain3.436@gmail.com>
2020-06-01 23:58:00 +0530

    Add solution for problem `B - Maximums` Link: https://codeforces.com/contest/1326/problem/B Tags: implementation, math, *900 Ref: https://codeforces.com/contest/1326/submission/82247387

---
+++ codeforces/1326/B.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+#define int                          long long
+#define mod                          1000000007  // 1e9 + 7 >>>  ans%mod
+#define greator_sort(arr,n)          sort(arr, arr+n, greater<int>())  //descending order sorting of array
+#define mk(arr,n,type)               type*arr = new type[n];    //to make array dynamically // n = size_of_array  |  type = int or long long etc;                      
+#define w(x)                         int x;cin>>x; while(x--)   //x = test cases
+#define pb                           push_back
+
+void c_p_c()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+
+int32_t main()
+{
+	c_p_c();
+	int n; cin >> n;
+	int b[n], a[n];
+	for (int i = 0; i < n; i++)
+		cin >> b[i];
+	int x = 0;
+	a[0] = b[0];
+	cout << a[0] << " ";
+	for (int i = 0; i < n; i++) {
+		if (i == 0)
+			continue;
+		x = max(x, a[i - 1]);
+		a[i] = b[i] + x;
+		cout << a[i] << " ";
+	}
+}
+
