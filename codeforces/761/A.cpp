d67714b6ab60a07ca92559adbc47ad38a48670d8
Yash Jain <yjain3.436@gmail.com>
2020-06-08 17:56:00 +0530

    Add solution for problem `A - Dasha and Stairs` Link: https://codeforces.com/contest/761/problem/A Tags: brute force, constructive algorithms, implementation, math, *1000 Ref: https://codeforces.com/contest/761/submission/83134243

---
+++ codeforces/761/A.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+#define int                          long long
+#define mod                          1000000007  // 1e9 + 7 >>>  ans%mod
+#define greator_sort(arr,n)          sort(arr, arr+n, greater<int>())  //descending order sorting of array
+#define mk(arr,n,type)               type*arr = new type[n];    //to make array dynamically // n = size_of_array  |  type = int or long long etc;                      
+#define w(x)                         int x;cin>>x; while(x--)   //x = test cases
+#define pb                           push_back
+
+void c_p_c()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+
+int32_t main()
+{
+	c_p_c();
+	int a, b; cin >> a >> b;
+	if (a == 0 and b == 0)
+		cout << "NO" << endl;
+	else if (abs(a - b) <= 1)
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+}
+
