aec548a19acfd8a951d229141ff721b3f6e69bce
Yash Jain <yjain3.436@gmail.com>
2020-06-18 00:29:00 +0530

    Add solution for problem `A - Short Substrings` Link: https://codeforces.com/contest/1367/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/1367/submission/84105692

---
+++ codeforces/1367/A.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+#define int                          long long
+#define mod                          1000000007  // 1e9 + 7 >>>  ans%mod
+#define greator_sort(arr,n)          sort(arr, arr+n, greater<int>())  //descending order sorting of array
+#define mk(arr,n,type)               type*arr = new type[n];    //to make array dynamically // n = size_of_array  |  type = int or long long etc;                      
+#define w(x)                         int x;cin>>x; while(x--)   //x = test cases
+#define pb                           push_back
+
+void c_p_c()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+
+int32_t main()
+{
+	c_p_c();
+	w(t)
+	{
+		string b;
+		cin >> b;
+		int l = b.size();
+		cout << b[0];
+		for (int i = 1; i < l; i = i + 2)
+		{
+			cout << b[i];
+		}
+		cout << endl;
+	}
+}
+
