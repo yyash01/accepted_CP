9437569c4fdab27aed55f05ae7ada028343f7971
Yash Jain <yjain3.436@gmail.com>
2020-07-02 16:29:00 +0530

    Add solution for problem `A - Magical Sticks` Link: https://codeforces.com/contest/1371/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1371/submission/85763407

---
+++ codeforces/1371/A.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+#define int                          long long
+#define mod                          1000000007  // 1e9 + 7 >>>  ans%mod
+#define greator_sort(arr,n)          sort(arr, arr+n, greater<int>())  //descending order sorting of array
+#define mk(arr,n,type)               type*arr = new type[n];    //to make array dynamically // n = size_of_array  |  type = int or long long etc;                      
+#define w(x)                         int x;cin>>x; while(x--)   //x = test cases
+#define pb                           push_back
+
+void c_p_c()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+
+int32_t main()
+{
+	//He wants to create the maximum number of sticks that have 
+	//the same length.
+	c_p_c();
+	w(t)
+	{
+		int n;cin>>n;
+		if(n%2==0)
+			cout<<n/2<<endl;
+		else
+			cout<< (n/2 + 1)<<endl;
+	}
+}
+
