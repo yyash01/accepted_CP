9ab7056344f020941ee984a7fa7fcd62cf73496c
Yash Jain <yjain3.436@gmail.com>
2020-07-16 14:32:00 +0530

    Add solution for problem `B - Shooting` Link: https://codeforces.com/contest/1216/problem/B Tags: greedy, implementation, sortings, *900 Ref: https://codeforces.com/contest/1216/submission/86972257

--- codeforces/1216/B.cpp
+++ codeforces/1216/B.cpp
@@ -1,31 +1,14 @@
 #include<bits/stdc++.h>
 #include<iostream>
-#include<math.h>
 using namespace std;
-
 #define int                          long long
 #define mod                          1000000007  // 1e9 + 7 >>>  ans%mod
 #define greator_sort(arr,n)          sort(arr, arr+n, greater<int>())  //descending order sorting of array
 #define mk(arr,n,type)               type*arr = new type[n];    //to make array dynamically // n = size_of_array  |  type = int or long long etc;                      
 #define w(x)                         int x;cin>>x; while(x--)   //x = test cases
-//---------------------------------------------------------------------------------------------------------------------------------------------------------//
-#define PB                           push_back
-#define MP                          make_pair
-#define vi                           vector<int>
-#define pii                          pair<int,int>
-#define mii                          map<int,int>
-#define rep(c)                       c.begin() , c.end()
-#define gcd(a,b)                     __gcd(a,b)
-#define lcm(a,b)                     (a*b)/gcd(a,b)
-//--------------------------------------------------------------------------------------------------------------------------------------------//
-#define F                            first
-#define S                            second
-//------------------------------------------------------------------------------------------------------------------------------------------------//
-#define FOR(n)                       for(int i=0;i<n;i++) //increasing for loop
-#define FORD(n)                      for(int i=n-1;i>=0;i--) //decreasing for loop
-//---------------------------------------------------------------------------------------------------------------------------------------//
-
-void booster()
+#define pb                           push_back
+#define rep(n)                       for(int i=0;i<n;i++)
+void c_p_c()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #ifndef ONLINE_JUDGE
@@ -34,43 +17,33 @@ void booster()
 #endif
 }
 
-bool sortinrev(const pair<int, int> &a,
-               const pair<int, int> &b)
+bool great_sort(const pair<int, int>&a, const pair<int, int>&b)
 {
 	return (a.first > b.first);
 }
 
 
-void solve()
-{
-	int n; cin >> n;
-
-	vector<pair<int , int>> ele;
-	//a[i] durability of the ith can
-	FOR(n)
-	{
-		int x; cin >> x;
-		ele.PB(MP(x, i + 1));
-	}
-	//first sort in decreasing order
-	sort(ele.begin(), ele.end(), sortinrev);
-	int sum = 0;
-	FOR(n)
-	{
-		sum += ele[i].F * i;
-	}
-	cout << sum + n << endl;
-	FOR(n) {cout << ele[i].S << " ";}
-
-
-
-}
 int32_t main()
 {
-	booster();
-	// w(t)
-	{
-		solve();
+	c_p_c();
+	int n; cin >> n;
+	int a[n];
+	vector<pair<int, int>>ind;
+	rep(n) {
+		cin >> a[i];
+		ind.pb(make_pair(a[i], i + 1));
 	}
+	int sum = 0;
+	//a[i]==durability of cans
+	sort(ind.begin(), ind.end(), great_sort);
+	for (int i = 0; i < n; i++)
+	{
+		sum = sum + (((ind[i].first) * i) + 1);
+	}
+	cout << sum << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << ind[i].second << " ";
+	}
+	cout << endl;
 }
-
