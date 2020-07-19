c04887b013a1b3706e8216c15f4fc1515fc800f6
Yash Jain <yjain3.436@gmail.com>
2020-07-19 21:00:00 +0530

    Add solution for problem `B - Restore the Permutation by Merger` Link: https://codeforces.com/contest/1385/problem/B Tags: greedy, *800 Ref: https://codeforces.com/contest/1385/submission/87367607

---
+++ codeforces/1385/B.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+#define int                          long long
+#define mod                          1000000007  // 1e9 + 7 >>>  ans%mod
+#define greator_sort(arr,n)          sort(arr, arr+n, greater<int>())  //descending order sorting of array
+#define mk(arr,n,type)               type*arr = new type[n];    //to make array dynamically // n = size_of_array  |  type = int or long long etc;                      
+#define w(x)                         int x;cin>>x; while(x--)   //x = test cases
+#define pb                           push_back
+#define rep(n)                       for(int i=0;i<n;i++)
+#define make_pair                    mk
+#define first                        f
+#define second                       sec
+void c_p_c()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+/*
+1st.COMMENT
+------------SORTING IN DESCENDING ORDER FOR VECTOR PAIR------------
+-----------ACCORDING TO FIRST ELEMENT IN PAIR---------------------
+
+bool great_sort(const pair<int, int>&a, const pair<int, int>&b)
+{
+	return (a.first > b.first);
+}
+
+------------------------------------------------------------------
+--------------------------------------------------------------------
+*/
+
+int32_t main()
+{
+	c_p_c();
+	w(t)
+	{
+		int n; cin >> n;
+		int k = 2 * n;
+		int a[k];
+		rep(k) {
+			cin >> a[i];
+		}
+		vector<int>arr;
+		for (int i = 0; i < k; i++)
+		{
+			if (i == 0) {
+				arr.pb(a[i]);
+				continue;
+			}
+			else
+			{
+				auto it = find(arr.begin(), arr.end(), a[i]);
+				if (it == arr.end())
+				{
+					arr.pb(a[i]);
+				}
+			}
+		}
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+	}
+}
