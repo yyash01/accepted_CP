9639887d0e4d5f91678dc3ebdd056ce4bc420713
Yash Jain <yjain3.436@gmail.com>
2020-07-19 16:07:00 +0530

    Add solution for problem `A - Three Pairwise Maximums` Link: https://codeforces.com/contest/1385/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1385/submission/87331680

---
+++ codeforces/1385/A.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+#define int                          long long
+#define mod                          1000000007  // 1e9 + 7 >>>  ans%mod
+#define greator_sort(arr,n)          sort(arr, arr+n, greater<int>())  //descending order sorting of array
+#define mk(arr,n,type)               type*arr = new type[n];    //to make array dynamically // n = size_of_array  |  type = int or long long etc;                      
+#define w(x)                         int x;cin>>x; while(x--)   //x = test cases
+#define pb                           push_back
+#define rep(n)                       for(int i=0;i<n;i++)
+#define make_pair                    mk
+#define first                        f
+#define second                       sec
+void c_p_c()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+/*
+1st.COMMENT
+------------SORTING IN DESCENDING ORDER FOR VECTOR PAIR------------
+-----------ACCORDING TO FIRST ELEMENT IN PAIR---------------------
+
+bool great_sort(const pair<int, int>&a, const pair<int, int>&b)
+{
+	return (a.first > b.first);
+}
+
+------------------------------------------------------------------
+--------------------------------------------------------------------
+*/
+
+int32_t main()
+{
+	c_p_c();
+	w(t)
+	{
+		int x, y, z; cin >> x >> y >> z;
+		if (x == y and y == z)
+		{
+			cout << "YES" << endl;
+			cout << x << " " << y << " " << z << endl;
+		}
+		else if (x == y and x > z)
+		{
+			cout << "YES" << endl;
+			cout << x << " " << z << " " << 1 << endl;
+		}
+		else if (x == z and x > y)
+		{
+			cout << "YES" << endl;
+			cout << y << " " << x << " " << 1 << endl;
+		}
+		else if (z == y and y > x)
+		{
+			cout << "YES" << endl;
+			cout << x << " " << 1 << " " << z << endl;
+		}
+		else {
+			cout << "NO" << endl;
+		}
+
+	}
+
+}
