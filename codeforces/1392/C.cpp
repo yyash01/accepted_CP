d44875eecb478c19cf0b1940aeab3ecbafec15f7
Yash Jain <yjain3.436@gmail.com>
2020-08-24 20:01:00 +0530

    Add solution for problem `C - Omkar and Waterslide` Link: https://codeforces.com/contest/1392/problem/C Tags: greedy, implementation, *1200 Ref: https://codeforces.com/contest/1392/submission/90834528

---
+++ codeforces/1392/C.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+#define int                          long long
+#define mod                          1000000007  // 1e9 + 7 >>>  ans%mod
+#define greator_sort(arr,n)          sort(arr, arr+n, greater<int>())  //descending order sorting of array
+#define mk(arr,n,type)               type*arr = new type[n];    //to make array dynamically // n = size_of_array  |  type = int or long long etc;                      
+#define w(x)                         int x;cin>>x; while(x--)   //x = test cases
+//---------------------------------------------------------------------------------------------------------------------------------------------------------//
+#define PB                           push_back
+#define MP                          make_pair
+#define vi                           vector<int>
+#define pii                          pair<int,int>
+#define mii                          map<int,int>
+#define rep(c)                       c.begin() , c.end()
+#define gcd(a,b)                     __gcd(a,b)
+#define lcm(a,b)                     (a*b)/gcd(a,b)
+//--------------------------------------------------------------------------------------------------------------------------------------------//
+#define F                            first
+#define S                            second
+//------------------------------------------------------------------------------------------------------------------------------------------------//
+#define FOR(n)                       for(int i=0;i<n;i++) //increasing for loop
+#define FORD(n)                      for(int i=n-1;i>=0;i--) //decreasing for loop
+//---------------------------------------------------------------------------------------------------------------------------------------//
+
+void booster()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+void solve()
+{
+	int n; cin >> n;
+
+	// making bool array
+	bool check[n] = {false};
+
+
+	int a[n]; FOR(n) {cin >> a[i];}
+	if (n == 1) {
+		cout << "0" << endl;
+		return;
+	}
+	int ans = 0;
+	for (int i = 1; i < n; i++)
+	{
+		if (a[i - 1] > a[i])
+		{
+			ans += abs(a[i - 1] - a[i]);
+		}
+	}
+	// or (int i = 0; i < n - 1; i++)
+	// {
+	// 	// else
+	// 	{
+	// 		if (check[i] == true and check[i + 1] == true)
+	// 		{
+	// 			ans += abs(a[i + 1] - a[i]);
+	// 		}
+	// 	}
+	// }
+	cout << ans << endl;
+}
+int32_t main()
+{
+	booster();
+	w(t)
+	{
+		solve();
+	}
+}
+
