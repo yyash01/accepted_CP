096778f0475d6b85b4d5fbeaea6e5d6caf226c44
Yash Jain <yjain3.436@gmail.com>
2020-10-25 13:01:00 +0530

    Add solution for problem `A - Reorder` Link: https://codeforces.com/contest/1436/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1436/submission/96633216

---
+++ codeforces/1436/A.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+
+#define inf                          (long long) 1e18
+#define int                          long long
+#define mod                          1000000007  // 1e9 + 7 >>>  ans%mod
+#define greator_sort(arr,n)          sort(arr, arr+n, greater<int>())  //descending order sorting of array
+#define mk(arr,n,type)               type*arr = new type[n];    //to make array dynamically // n = size_of_array  |  type = int or long long etc;                      
+#define w(x)                         int x;cin>>x; while(x--)   //x = test cases
+//---------------------------------------------------------------------------------------------------------------------------------------------------------//
+#define PB                           push_back
+#define MP                          make_pair
+#define vi                           vector<int>
+#define pii                          pair<int,int>
+#define mii                          map<int,int>
+#define rep(c)                       c.begin() , c.end()
+#define gcd(a,b)                     __gcd(a,b)
+#define lcm(a,b)                     (a*b)/gcd(a,b)
+//--------------------------------------------------------------------------------------------------------------------------------------------//
+#define F                            first
+#define S                            second
+//------------------------------------------------------------------------------------------------------------------------------------------------//
+#define FOR(n)                       for(int i=0;i<n;i++) //increasing for loop
+#define FORD(n)                      for(int i=n-1;i>=0;i--) //decreasing for loop
+//---------------------------------------------------------------------------------------------------------------------------------------//
+
+void booster()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+void solve()
+{
+	int n, m; cin >> n >> m;
+	int a[n];
+	int sum = 0;
+	FOR(n)
+	{
+		int x ;
+		cin >> x;
+		sum += x;
+	}
+	if (sum == m)
+	{
+		cout << "YES" << "\n";
+	}
+	else
+	{
+		cout << "NO" << "\n";
+	}
+}
+int32_t main()
+{
+	booster();
+	w(t)
+	{
+		solve();
+	}
+}
+
+
