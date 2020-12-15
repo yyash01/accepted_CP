24441b155e811f5dfe0bdd579ea80c3c2883a1ce
Yash Jain <yjain3.436@gmail.com>
2020-12-15 23:48:00 +0530

    Add solution for problem `C - Ping-pong` Link: https://codeforces.com/contest/1455/problem/C Tags: constructive algorithms, games, math, *1100 Ref: https://codeforces.com/contest/1455/submission/101357938

---
+++ codeforces/1455/C.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+
+#define inf                          (long long) 1e18
+#define int                          long long
+#define mod                          1000000007  // 1e9 + 7 >>>  ans%mod
+#define greator_sort(arr,n)          sort(arr, arr+n, greater<int>())  //descending order sorting of array
+#define mk(arr,n,type)               type*arr = new type[n];    //to make array dynamically // n = size_of_array  |  type = int or long long etc;                      
+#define w(x)                         int x;cin>>x; while(x--)   //x = test cases
+//---------------------------------------------------------------------------------------------------------------------------------------------------------//
+#define PB                           push_back
+#define MP                          make_pair
+#define vi                           vector<int>
+#define pii                          pair<int,int>
+#define mii                          map<int,int>
+#define rep(c)                       c.begin() , c.end()
+#define gcd(a,b)                     __gcd(a,b)
+#define lcm(a,b)                     (a*b)/gcd(a,b)
+//--------------------------------------------------------------------------------------------------------------------------------------------//
+#define F                            first
+#define S                            second
+//------------------------------------------------------------------------------------------------------------------------------------------------//
+#define FOR(n)                       for(int i=0;i<n;i++) //increasing for loop
+#define FORD(n)                      for(int i=n-1;i>=0;i--) //decreasing for loop
+//---------------------------------------------------------------------------------------------------------------------------------------//
+
+void booster()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+void solve()
+{
+	int x, y; cin >> x >> y;
+	if (x == 1 and x <= y)
+	{
+		cout << 0 << " " << y << "\n";
+		return;
+	}
+	cout << x - 1 << " " << y  << "\n";
+}
+int32_t main()
+{
+	booster();
+	w(t)
+	{
+		solve();
+	}
+}
+
+
