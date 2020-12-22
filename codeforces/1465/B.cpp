9a21dba60ec11ff72b2ca5af1eec3282fde35505
Yash Jain <yjain3.436@gmail.com>
2020-12-22 23:09:00 +0530

    Add solution for problem `B - Fair Numbers` Link: https://codeforces.com/contest/1465/problem/B Tags: brute force, math, number theory, *1000 Ref: https://codeforces.com/contest/1465/submission/102065398

---
+++ codeforces/1465/B.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+
+#define inf                          (long long) 1e18
+#define int                          long long
+#define mod                          1000000007  // 1e9 + 7 >>>  ans%mod
+#define greator_sort(arr,n)          sort(arr, arr+n, greater<int>())  //descending order sorting of array
+#define mk(arr,n,type)               type*arr = new type[n];    //to make array dynamically // n = size_of_array  |  type = int or long long etc;                      
+#define w(x)                         int x;cin>>x; while(x--)   //x = test cases
+//---------------------------------------------------------------------------------------------------------------------------------------------------------//
+#define PB                           push_back
+#define MP                          make_pair
+#define vi                           vector<int>
+#define pii                          pair<int,int>
+#define mii                          map<int,int>
+#define rep(c)                       c.begin() , c.end()
+#define gcd(a,b)                     __gcd(a,b)
+#define lcm(a,b)                     (a*b)/gcd(a,b)
+//--------------------------------------------------------------------------------------------------------------------------------------------//
+#define F                            first
+#define S                            second
+//------------------------------------------------------------------------------------------------------------------------------------------------//
+#define FOR(n)                       for(int i=0;i<n;i++) //increasing for loop
+#define FORD(n)                      for(int i=n-1;i>=0;i--) //decreasing for loop
+//---------------------------------------------------------------------------------------------------------------------------------------//
+
+void booster()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+
+void solve()
+{
+	int n; cin >> n;
+	int ans = n;
+	while (true)
+	{
+		int pre = ans;
+		vector<int> v;
+		while (true)
+		{
+			if (ans == 0)
+			{
+				break;
+			}
+			if (ans % 10 != 0) // the digit is non zero will enter vector
+			{
+				v.PB(ans % 10);
+			}
+			ans = ans / 10;
+		}
+		int flag = 1;
+		for (int i = 0; i < v.size(); i++)
+		{
+			if (pre % v[i] != 0)
+			{
+				flag = 0;
+				break;
+			}
+		}
+		if (flag)
+		{
+			cout << pre << "\n";
+			break;
+		}
+		ans = pre + 1;
+	}
+}
+int32_t main()
+{
+	booster();
+	w(t)
+	{
+		solve();
+	}
+}
+
+
